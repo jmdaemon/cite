5ae9e8fb6b4f65f305642f9ab41dff1cb15b7907
Joseph Diza <josephm.diza@gmail.com>
2022-04-19 14:02:56 -0700

    Remove req request code

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
--- For Req Network Library
---{-# LANGUAGE DeriveGeneric     #-}
 
 module Main where
 
 import Lib
 import System.Environment
 import System.Exit
-
--- Req Network Get Request
---import Control.Monad
---import Control.Monad.IO.Class
---import Data.Aeson
---import Data.Maybe (fromJust)
---import Data.Monoid ((<>))
---import Data.Text (Text)
---import Data.Text as DataT
---import GHC.Generics
---import Network.HTTP.Req
---import qualified Data.ByteString.Char8 as B
---import qualified Text.URI as URI
-
---main :: IO ()
---main = runReq defaultHttpConfig $ do
-    --args <- liftIO getArgs 
-    --let url = DataT.pack $ Prelude.head $ Prelude.take 1 args
-    --bs <-
-        --req
-            --GET
-            --(https url)
-            ----url
-            --NoReqBody
-            --bsResponse
-            --mempty
-    --liftIO $ B.putStrLn (responseBody bs)
-
--- Wreq Network Get Request
 import Network.Wreq
 import Control.Lens
 import Data.ByteString.Lazy.Char8 as BS
