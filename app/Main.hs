e7c5f3c8c3b713dccb6b85d146d38f7cec51ddbd
Joseph Diza <josephm.diza@gmail.com>
2022-04-19 13:37:57 -0700

    Successfully send some http requests using req hackage library
    Note that this library works in a very funny way and does the
    construction of url schemes in a very quirky way. You cannot use the
    fully qualified names which make it confusing.

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,51 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric     #-}
+
 module Main where
 
 import Lib
 import System.Environment
 import System.Exit
-import Network.Wreq
-import Control.Lens
+--import Network.Wreq
+
+--import Control.Lens
+--import Control.Monad.IO.Class
+--import Data.Aeson
+--import Network.HTTP.Req
+
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Aeson
+import Data.Maybe (fromJust)
+import Data.Monoid ((<>))
+import Data.Text (Text)
+import Data.Text as DataT
+import GHC.Generics
+import Network.HTTP.Req
+import qualified Data.ByteString.Char8 as B
+import qualified Text.URI as URI
+
+main :: IO ()
+main = runReq defaultHttpConfig $ do
+    args <- liftIO getArgs 
+    --args <- getArgs
+        --let url = take 1 args
+    --let url = take 1 <- getArgs
+    --args <- getArgs
+    --let url = DataT.pack $ Prelude.head $ Prelude.take 1 args
+    --let url = DataT.fromString $ Prelude.head $ Prelude.take 1 args
+    let url = DataT.pack $ Prelude.head $ Prelude.take 1 args
+    --r <-
+    bs <-
+        req
+            GET
+            (https url)
+            --url
+            NoReqBody
+            bsResponse
+            mempty
+    liftIO $ B.putStrLn (responseBody bs)
+    --liftIO $ print (responseBody r :: Value)
 
 --main = getArgs >>= parse >>= putStr . cite
 --main = getArgs >>= parse >>= cite
@@ -14,13 +54,25 @@ import Control.Lens
 --main = getArgs >>= cite . parse
 
 --main = getArgs >>= parse >>= cite
-main = do 
-    args <- getArgs
-    print args
 
-showResponse response = print (response ^. responseBody) 
 
---cite lines = unlines . reverse . lines
+
+--showResponseBody response = print (response ^. responseBody)
+----showResponseBody response = print (response ^. responseHeader "Content-Type")
+
+--main = do 
+    --args <- getArgs
+    --fmap  (mapM $ showResponseBody) (mapM get args)
+
+
+
+    --fmap (mapM showResponseBody) (mapM get args)
+    --mapM print args
+    --fmap print (mapM get args)
+
+--showResponse response = print (response ^. responseBody) 
+
+--cite lin(es = unlines . reverse . lines
 --cite responses = fmap . print (response ^. responseBody)
 --cite responses = map ($ showResponse) responses 
 --cite urls = map ($ showResponse) (map (get) urls)
